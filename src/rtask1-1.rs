4452dcd5d54fd9d620972f77dd478ac8c00ff5d1
e-leshnik <evelinaat33@yandex.com>
2025-10-23 23:08:58 +0300

    Added the code that sums two intsand prints the result

---
+++ src/rtask1-1.rs
@@ -0,0 +1,12 @@
+use std::io;
+
+fn main() {
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).unwrap();
+    
+    let mut iter = input.split_whitespace();
+    let a: i32 = iter.next().unwrap().parse().unwrap();
+    let b: i32 = iter.next().unwrap().parse().unwrap();
+    
+    println!("{}", a + b);
+}
